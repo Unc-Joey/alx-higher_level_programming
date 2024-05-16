05ec1caa8fbaa5e28fbd429651c6c20e6e373904
Unc-Joey <josephsekhukhune98@gmail.com>
2024-05-16 11:22:46 -0700

    1-calculation.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    from add_0 import add
-    a=1
-    b=2
-    print("{} + {} = {}".formart(a, b, add(a, b)))
+a = 1
+b = 2
+from add_0 import add
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+# Define variables a and b on separate lines
+a = 10
+b = 5
+
+# Import required functions from calculator_1.py
+from calculator_1 import add, sub, mul, div
+
+# Perform calculations using the imported functions
+addition_result = add(a, b)
+subtraction_result = sub(a, b)
+multiplication_result = mul(a, b)
+division_result = div(a, b)
+
+# Print the results, ensuring no more than four print calls
+print(f"{a} + {b} = {addition_result}")
+print(f"{a} - {b} = {subtraction_result}")
+print(f"{a} * {b} = {multiplication_result}")
+print(f"{a} / {b} = {division_result}")
+
